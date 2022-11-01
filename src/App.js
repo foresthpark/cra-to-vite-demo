37d72a19117daa9f13d9140779fc150e82ee6695
Forest Park <forestpark@Forests-MBP.cipherkey.com>
2022-11-01 16:52:06 -0600

    lol

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p className="text-5xl">
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
